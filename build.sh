ab6aab6525f6b5daf8d8a3baa88e227b30496a3f
Marco <marco.volpato00@gmail.com>
2019-11-05 16:16:52 +0100

    WIP multiarch build

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t marcovolpato00/dnsmasq-webproc:latest .
+docker buildx build --platform linux/amd64,linux/arm/v7,linux/arm64 -t marcovolpato00/dnsmasq-webproc:latest . --push
+# docker build -t marcovolpato00/dnsmasq-webproc:latest .
